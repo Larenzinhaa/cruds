4298572eb9e5e316db13ba48197041095118993c
Ana Lauren Dourado <analaurendourado@gmail.com>
2024-11-05 14:15:25 -0300

    Initial commit

---
+++ conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+    $servername = "localhost";
+    $username ="root";
+    $password ="";
+    $dbname ="crudS";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// if ($conn->connect_error){
+
+    // echo " Erro de conexão " . $conn->connect_error;
+
+// } else {
+    // echo "Conexão Bem-Sucedida!";
+// }
+
+?>
